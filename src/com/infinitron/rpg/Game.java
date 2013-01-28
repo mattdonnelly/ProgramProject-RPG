2a4127214246d17fe119ca6820f559d02feedb6e
mattdonnelly <mattdonnelly@me.com>
2013-01-28 21:37:11 +0000

    Something is not working here

--- src/com/infinitron/rpg/Game.java
+++ src/com/infinitron/rpg/Game.java
@@ -1,22 +1,23 @@
 package com.infinitron.rpg;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.view.View;
 
-public class Game extends Activity {
+public class Game extends View {
+	private Bitmap bitmap;
+ 
+	public Game(Context context) {
+		super(context);
+		bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_game, menu);
-        return true;
-    }
-    
+	@Override
+	protected void onDraw(Canvas canvas) {
+		canvas.drawColor(Color.BLACK);
+		canvas.drawBitmap(bitmap, 100, 100, null);
+	}
 }
