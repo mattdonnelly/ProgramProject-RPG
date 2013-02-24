c1a9cdf3a6e1cbf57d3489090b135609e5402ce5
Geoffrey Hodgins <geoffrey.hodgins@gmail.com>
2013-02-24 17:55:41 +0000

    Added sprite, fixed initialisation of item array
    Rendered a monster sprite to test for future use.

--- src/com/infinitron/rpg/Game.java
+++ src/com/infinitron/rpg/Game.java
@@ -13,9 +13,15 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
 	private GameRenderer renderer;
 	
 	// TESTING SPRITE RENDERING
+	//Player sprite rendering
 	public GridSpriteSheet elaine = new GridSpriteSheet(BitmapFactory.decodeResource(getResources(), R.drawable.walk_elaine), 30, 47);
 	public Sprite elaineSprite = new Sprite(elaine, 2);
 	public GameObject elaineObject = new GameObject("Elaine", elaineSprite, 20, 20);
+	
+	// Monster sprite rendering
+	public GridSpriteSheet monster1 = new GridSpriteSheet(BitmapFactory.decodeResource(getResources(), R.drawable.monster_inc), 100, 100);
+	public Sprite monster1Sprite = new Sprite(monster1, 2);
+	public GameObject monster1Object = new GameObject("Monster1", monster1Sprite, 110, 50);
 	
 	public Bitmap map = BitmapFactory.decodeResource(getResources(), R.drawable.map);
 	public GridSpriteSheet map_tilesheet = new GridSpriteSheet(BitmapFactory.decodeResource(getResources(), R.drawable.map_tilesheet), 16, 16); 
@@ -38,6 +44,7 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
 		
 		renderer = new GameRenderer();
 		renderer.addRenderable(elaineObject);
+		renderer.addRenderable(monster1Object);
 		
 		gameThread = new GameThread(this, renderer, level);
 	}
@@ -81,15 +88,15 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
 	
 	// 2d array of items, where the each row is a different type of item and the each column represents a different object of type item where the greater
 	// the column index the greater the item level
-	public void createItems(){
+	public void createItems(Sprite itemSprites[][]){
 		for(int j = 0; j < itemCollection.length; j++){
 			for(int i = 0; i < itemCollection[j].length; i++){
 				if(j == 0){
-					//itemCollection[j][i] = new Weapon();
+					itemCollection[j][i] = (Item) new Weapon("An Item", itemSprites[j][i], 0, 0, 0, 0, 0, 0);
 				}else if(j == 1){
-					//itemCollection[j][i] = new Armor();
+					itemCollection[j][i] = (Item) new Armor("A piece of Armour", itemSprites[j][i], 0, 0, 0, 0, 0, 0);
 				}else if(j == 2){
-					//itemCollection[j][i] = new Potion();
+					itemCollection[j][i] = (Item) new Potion("A Potion", itemSprites[j][i], 0, 0, 0, 0, 0, 0);
 				}
 			}
 		}
